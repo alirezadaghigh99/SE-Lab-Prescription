2289a41fa4376a176d454f6a511f83fe1c73d838
nima <nimxj4141@gmail.com>
2021-12-19 00:26:03 +0330

    event_handler added

---
+++ src/event_handler.py
@@ -0,0 +1,15 @@
+from event_bus import EventBus
+from models import db, Prescription
+
+bus = EventBus()
+
+
+@bus.on('create:prescription')
+def create_prescription_event_handler(event):
+    prescription = Prescription(id=event.prescription_id,
+                                doctor_id=event.prescription_doctor_id,
+                                patient_id=event.prescription_patient_id,
+                                drug=event.prescription_drug,
+                                comment=event.prescription_comment)
+    db.session.add(prescription)
+    db.session.commit()
