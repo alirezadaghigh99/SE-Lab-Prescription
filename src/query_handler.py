e19844dd7f64528d360e5df242a4d32699eb6af0
nima <nimxj4141@gmail.com>
2021-12-20 23:07:09 +0330

    updated for stats

--- src/query_handler.py
+++ src/query_handler.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request
 from flask.json import jsonify
 from models import Prescription
+from datetime import date
+from sqlalchemy import func
+from http import HTTPStatus
 
 query_handler = Blueprint('Query Handler', __name__)
 
@@ -14,4 +17,18 @@ def read_prescriptions():
         query = query.filter_by(patient_id=request.args['national_id'])
     if request.args["role"] == "admin":
         pass
+    return jsonify([prescription.to_dict() for prescription in query.all()])
+
+
+@query_handler.route('/prescription/stats', methods=['GET'])
+def prescriptions_stats():
+    query = Prescription.query
+    try:
+        day = int(request.args["day"])
+        month = int(request.args["month"])
+        year = int(request.args["year"])
+        date_obj = date(year, month, day)
+    except:
+        return jsonify({"message": "Bad request"}), HTTPStatus.BAD_REQUEST
+    query = query.filter(func.DATE(Prescription.timestamp) == date_obj)
     return jsonify([prescription.to_dict() for prescription in query.all()])
